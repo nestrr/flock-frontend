c293ec453942aa420b9ddad77b73740ff8b1aece
Maryam Khan <maryam.dev@outlook.com>
2025-03-12 22:13:24 -0700

    Use gradient background in header too, display error messages

--- app/shared/input-item.tsx
+++ app/shared/input-item.tsx
@@ -5,6 +5,9 @@ import {
   InputAddon,
   type InputProps,
   Text,
+  VStack,
+  Alert,
+  type AlertRootProps,
 } from "@chakra-ui/react";
 import React from "react";
 import { InputGroup } from "@/app/shared/snippets/input-group";
@@ -20,6 +23,8 @@ export type InputPropsType = Simplify<
     endAddon?: React.ReactNode;
     colorPalette?: string;
     placeholder?: string;
+    alert?: string;
+    alertStatus?: AlertRootProps["status"];
   } & InputProps
 >;
 export default function InputItem(inputProps: InputPropsType) {
@@ -30,48 +35,60 @@ export default function InputItem(inputProps: InputPropsType) {
     children,
     label,
     endAddon,
+    alert,
+    alertStatus,
     colorPalette = "accent.muted/50",
     ...rest
   } = inputProps;
   return (
-    <Group
-      attached
-      borderColor={colorPalette}
-      borderWidth={3}
-      borderStyle={"solid"}
-      rounded="lg"
-      w={"100%"}
-    >
-      <InputAddon bg={colorPalette} border={0}>
-        <Text
-          fontSize={"md"}
-          fontWeight={"bold"}
-          letterSpacing={"wide"}
-          lineHeight={"normal"}
-        >
-          {label}
-        </Text>
-      </InputAddon>
-      {!!children ? (
-        children
-      ) : (
-        <InputGroup flex="1" startElement={startElement}>
-          <Input
-            border={0}
-            readOnly={locked}
-            value={value}
-            w={"100%"}
-            rounded="none"
-            {...rest}
-          ></Input>
-        </InputGroup>
-      )}
-
-      {!!endAddon && (
+    <VStack w={"100%"} alignItems={"stretch"}>
+      <Group
+        attached
+        borderColor={colorPalette}
+        borderWidth={3}
+        borderStyle={"solid"}
+        rounded="lg"
+      >
         <InputAddon bg={colorPalette} border={0}>
-          {endAddon}
+          <Text
+            fontSize={"md"}
+            fontWeight={"bold"}
+            letterSpacing={"wide"}
+            lineHeight={"normal"}
+          >
+            {label}
+          </Text>
         </InputAddon>
+        {!!children ? (
+          children
+        ) : (
+          <InputGroup flex="1" startElement={startElement}>
+            <Input
+              border={0}
+              readOnly={locked}
+              value={value}
+              w={"100%"}
+              rounded="none"
+              {...rest}
+            ></Input>
+          </InputGroup>
+        )}
+
+        {!!endAddon && (
+          <InputAddon bg={colorPalette} border={0}>
+            {endAddon}
+          </InputAddon>
+        )}
+      </Group>
+      {alert && (
+        <Alert.Root
+          status={alertStatus ?? "error"}
+          bg={{ _light: "red.600/80" }}
+          color={{ _light: "white" }}
+        >
+          <Alert.Title> {alert} </Alert.Title>
+        </Alert.Root>
       )}
-    </Group>
+    </VStack>
   );
 }
